bff674bdf8aab27fce8c3c787a7eeb4b3aeb5ae8
진예진 <yeen6667@gmail.com>
2016-08-10 21:51:32 +0900

    why i can't generate window that similar...

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 # -*- coidng: utf-8 -*-
 
+import window
+from tkinter import *
+
+main_win = Tk()
+main_win.title ("Project")
+
+window.gen_entry(main_win)
+window.gen_button(main_win)
+window.gen_range_button(main_win)
+window.gen_display(main_win)
